57cdca54d5fa022fd7e8b40e8538ca0251c5a8f4
JuhwanLee99 <jimmy3470second@gmail.com>
2026-01-27 15:57:54 +0900

    feat: gitignore 업데이트 및 인증페이지 추가

--- src/shared/firebase/client.ts
+++ src/shared/firebase/client.ts
@@ -1,4 +1,5 @@
-import { FirebaseOptions, getApp, getApps, initializeApp } from 'firebase/app';
+import { getApp, getApps, initializeApp } from 'firebase/app';
+import type { FirebaseOptions } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig: FirebaseOptions = {
